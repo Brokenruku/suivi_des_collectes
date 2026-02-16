524da64fabc605929b511b602cdeade4ff068261
Brokenruku <168399819+Brokenruku@users.noreply.github.com>
2026-02-16 14:15:08 +0300

    pdo

---
+++ app/models/connection.php
@@ -0,0 +1,19 @@
+<?php
+$host = 'localhost';
+$dbname = '4064_4078_4107';
+$username = 'postgres';
+$password = 'cristelle31';
+$port = '5432'; 
+
+try {
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;";
+    
+    $pdo = new PDO($dsn, $username, $password);
+    
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+?>
