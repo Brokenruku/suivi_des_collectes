57cbb1e8404c75851ecfc091726658b5c1a5776e
DahKoul <andriamanampisoakoloina@gmail.com>
2026-02-16 11:00:53 +0300

    sqellette1

--- app/config/routes.php
+++ app/config/routes.php
@@ -16,18 +16,8 @@ $router->group('', function (Router $router) {
     Flight::redirect('/login');
   });
 
-  $router->get('/objects', [
-    ObjectController::class, 'index']);
-  $router->get('/objects/@id', [ObjectController::class, 'show']);
-
-  $router->post('/echanges/proposer', [ObjectController::class, 'proposer']);
-  $router->get('/echanges', [ObjectController::class, 'echanges']);
-  $router->post('/echanges/@id/refuser', [ObjectController::class, 'refuser']);
-  $router->post('/echanges/@id/accepter', [ObjectController::class, 'accepter']);
-
   $router->get('/login', function () {
     Flight::render('login');
   });
 
-  $router->post('/login', [ObjectController::class, 'login']);
 }, [SecurityHeadersMiddleware::class]);
