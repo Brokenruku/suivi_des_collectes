92503094dd66ba829386b375781c3fded36a2975
Brokenruku <168399819+Brokenruku@users.noreply.github.com>
2026-02-17 16:11:44 +0300

    base mr dons

--- insertion.sql
+++ insertion.sql
@@ -66,3 +66,43 @@ INSERT INTO besoin_argent (vola, id_ville) VALUES
 
 INSERT INTO reduction_vente (pourcentage) VALUES
 (10);
+
+INSERT INTO dons (id, id_users, date) VALUES
+(1, 1, '2026-02-16'),
+(2, 1, '2026-02-16'),
+(3, 1, '2026-02-17'),
+(4, 1, '2026-02-17'),
+(5, 1, '2026-02-17'),
+(6, 1, '2026-02-16'),
+(7, 1, '2026-02-16'),
+(8, 1, '2026-02-17'),
+(9, 1, '2026-02-17'),
+(10, 1, '2026-02-17'),
+(11, 1, '2026-02-18'),
+(12, 1, '2026-02-18'),
+(13, 1, '2026-02-18'),
+(14, 1, '2026-02-19'),
+(15, 1, '2026-02-19'),
+(16, 1, '2026-02-17');
+
+INSERT INTO dons_argent (id_dons, vola) VALUES
+(1, 5000000),
+(2, 3000000),
+(3, 4000000),
+(4, 1500000),
+(5, 6000000),
+(14, 20000000);
+
+INSERT INTO dons_nature (id_dons, id_objet_nature, qte) VALUES
+(6,  (SELECT id FROM objet_nature WHERE nom='Riz (kg)' LIMIT 1), 400),
+(7,  (SELECT id FROM objet_nature WHERE nom='Eau (L)' LIMIT 1), 600),
+(10, (SELECT id FROM objet_nature WHERE nom='Haricots' LIMIT 1), 100),
+(11, (SELECT id FROM objet_nature WHERE nom='Riz (kg)' LIMIT 1), 2000),
+(13, (SELECT id FROM objet_nature WHERE nom='Eau (L)' LIMIT 1), 5000),
+(16, (SELECT id FROM objet_nature WHERE nom='Haricots' LIMIT 1), 88);
+
+INSERT INTO dons_materiaux (id_dons, id_objet_materiaux, qte) VALUES
+(8,  (SELECT id FROM objet_materiaux WHERE nom='Tôle' LIMIT 1), 50),
+(9,  (SELECT id FROM objet_materiaux WHERE nom='Bâche' LIMIT 1), 70),
+(12, (SELECT id FROM objet_materiaux WHERE nom='Tôle' LIMIT 1), 300),
+(15, (SELECT id FROM objet_materiaux WHERE nom='Bâche' LIMIT 1), 500);
